ede1d45b8b49debd28a14781037c54507ff6b445
sweetcoin-ai <v.mpb@icloud.com>
2025-07-09 18:43:20 -0400

    Add files via upload

---
+++ firebase-init-merchant.js
@@ -0,0 +1,21 @@
+// Firebase initialization for merchant use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkAVCai3qijzM1y0PS9eJeyTZcB9Ov15c",
+  authDomain: "sweetcoin-b4af9.firebaseapp.com",
+  projectId: "sweetcoin-b4af9",
+  storageBucket: "sweetcoin-b4af9.appspot.com",
+  messagingSenderId: "258870437381",
+  appId: "1:258870437381:web:36b90c86ae39e63267ce8e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc };
