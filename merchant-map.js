f983374c215235df9602f7617ef0b370f6442a1e
sweetcoin-ai <v.mpb@icloud.com>
2025-07-10 10:51:26 -0400

    Update merchant-map.js

--- merchant-map.js
+++ merchant-map.js
@@ -5,18 +5,11 @@ import { collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
 const ref = collection(db, "merchants");
 console.log("ref test:", ref); // 看看是否 silent fail
 
-import { db } from './firebase-init-merchant.js';
-
-
 
 const map = L.map('map').setView([43.65107, -79.347015], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map)；
-
-console.log("📡 Firebase db:", db);
-const testRef = collection(db, "merchants");
-console.log("📌 collection ref:", testRef);
 
 map.on('click', async (e) => {
     const name = prompt("Enter your shop name:");
